9812b3e4089a9ec61e222936d439baec310d1cf0
Roland-Hufnagel <roland.hufnagel@gmx.de>
2022-10-20 21:12:35 +0200

    finish survey-creation

---
+++ pages/create/index.js
@@ -0,0 +1,122 @@
+import { useState } from "react";
+
+import styled from "styled-components";
+import AddButton from "../../components/Buttons/AddButton";
+import DeleteButton from "../../components/Buttons/DeleteButton";
+
+export default function CreateSurvey() {
+  const [inputFields, setInputFields] = useState([
+    {
+      question: "",
+    },
+  ]);
+
+  const handleChangeInput = (index, event) => {
+    const values = [...inputFields];
+    values[index].question = event.target.value;
+    setInputFields(values);
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  
+  function handleDelete(index) {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  }
+  function handleAdd() {
+    setInputFields((pre) => [...pre, { question: "" }]);
+  }
+
+  return (
+    <Container>
+      <h1>Create your personal survey:</h1>
+
+      <form onSubmit={handleSubmit}>
+        <AddButton onClick={handleAdd} />
+        {inputFields.map((inputField, index) => (
+          <MyWrapper key={index}>
+            <input
+              onKeyPress={(e) => {
+                e.key === "Enter" && e.preventDefault();
+              }}
+              maxLength="200"
+              type="text"
+              name="question"
+              placeholder="your question"
+              value={inputField.question}
+              onChange={(event) => handleChangeInput(index, event)}
+            />
+
+            <DeleteButton
+              onClick={() => {
+                handleDelete(index);
+              }}
+            />
+
+            <MyBox>
+              <legend>Preview:</legend>
+              <p>{inputField.question}</p>
+              <input type="radio" disabled />
+              Yes
+              <input type="radio" disabled />
+              No
+            </MyBox>
+          </MyWrapper>
+        ))}<hr/>
+        <SubmitButton type="submit">Save</SubmitButton>
+      </form>
+    </Container>
+  );
+}
+
+const MyWrapper = styled.div`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: 9fr auto;
+  & input {
+    border: 1px solid #ccc;
+    padding: 0.7em;
+    font-size: 1rem;
+    border-radius: 0.5em;
+  }
+  & input[type="radio"] {
+    margin-left: 20px;
+    margin-right: 10px;
+  }
+`;
+const Container = styled.main`
+  padding: 10px;
+  max-width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  & h1 {
+    text-align: center;
+  }
+`;
+const MyBox = styled.fieldset`
+  grid-column-start: 1;
+  grid-column-end: 3;
+  background-color: #ddd;
+  color: #666;
+  border-radius: 5px;
+  border: 1px solid #aaa;
+  margin-bottom: 30px;
+`;
+const SubmitButton = styled.button`
+  padding: 0.5em;
+  border: 4px solid #44803f;
+  border-radius: 0.4em;
+  font-size: 1.1em;
+  color: #44803f;
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+    color: #146152;
+    border: 4px solid #146152;
+  }
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,10 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>My App</h1>
+        <h1>Free Feedback Form</h1>
+        <Link href="/create">Create Survey</Link>
       </main>
     </div>
   );
 }
-
-
