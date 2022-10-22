58a3f57e44396526085cac3c1f1b68b0d2b9a90c
Roland-Hufnagel <roland.hufnagel@gmx.de>
2022-10-22 15:41:50 +0200

    add Delete Button to listview

--- pages/surveys/index.js
+++ pages/surveys/index.js
@@ -1,14 +1,29 @@
 import SurveyCard from "../../components/SurveyCard";
 import { getAllSurveys } from "../../services/surveyService";
 import styled from "styled-components";
+import { useState } from "react";
 
 export default function Surveys() {
-  const surveys = getAllSurveys();
+  const [surveys, setSurveys] = useState(getAllSurveys());
+
+  function handleDelete(index) {
+    console.log("handleDelete", index);
+    const newObj = [...surveys];
+    newObj.splice(index, 1);
+    setSurveys(newObj);
+  }
+ 
   return (
     <>
       <StyledSurveyList>
         {surveys.map((survey, index) => (
-          <SurveyCard key={index} title={survey.title} date={survey.date} id={survey.id}/>
+          <SurveyCard
+            key={index}
+            title={survey.title}
+            date={survey.date}
+            id={survey.id}
+            onDelete={() => handleDelete(index)}
+          />
         ))}
       </StyledSurveyList>
     </>
